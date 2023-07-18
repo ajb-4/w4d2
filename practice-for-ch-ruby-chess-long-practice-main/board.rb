f4d6ac56bc0c3417c71f6b19da0726d298a617aa
AJU <aulcena@gmail.com>
2023-07-18 18:06:45 -0400

    working move_piece

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,10 +1,30 @@
 class Board
     attr_accessor :rows
+
     def initialize
+
         @rows = Array.new(32){Array.new(32)}
         @null_piece = NullPiece.new
+
     end
 
+    def[](pos)
+        row,col = pos
+        @rows[row,col]
+    end
+
+    def[]=(pos,val)
+        row,col = pos
+        @rows[row,col] = val
+    end
+
+    def move_piece(start_pos,end_pos)
+
+        raise "There is no piece here" if @rows[start_pos].empty?
+
+        raise "Position already occupied" if !@rows[end_pos].empty?
+        
+    end
     
 
 
